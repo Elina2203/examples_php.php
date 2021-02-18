f136239703ed852bde4516a63e5d0c92055e1cb0
Elina2203 <elina.olsanska@gmail.com>
2021-02-18 10:58:29 +0200

    added example nine

--- examples.php
+++ examples.php
@@ -8,9 +8,10 @@
 for($col = 0; $col < 3; $col++){
     for($row = 0; $row < 3; $row++){
 
-        $entries[$row][$col] ='x';}
-        
+        $entries[$row][$col] ='x';
     }
+        
+}
 
 ?>
 <?php
@@ -19,13 +20,15 @@ for($col = 0; $col < 3; $col++){
     ['0', '0', '0'],
     ['0', '0', '0']
 ];
+
 for($col = 0; $col < 3; $col++){
     for($row = 0; $row < 3; $row++){
 
-        $entries[$row][$col] ='x';}
+        $entries[$row][$col] ='x';
+    }
 
         
-    }
+}
 
 ?>
 <?php output($entries);?>
@@ -56,10 +59,11 @@ for($col = 0; $col < 3; $col++){
 for($col = 1; $col < 3; $col++){
     for($row = 0; $row < 3; $row++){
 
-        $entries[$row][$col] ='x';}
+        $entries[$row][$col] ='x';
+    }
 
         
-    }
+}
 
 ?>
 <?php output($entries);?>
@@ -78,12 +82,13 @@ for($col = 1; $col < 3; $col++){
 for($col = 0; $col < 3; $col++){
     for($row = 0; $row < 3; $row++){
         if($row===1 && $col===1){
-        continue;
+            continue;
         }
-        $entries[$row][$col] ='x';}
+        $entries[$row][$col] ='x';
+    }
 
         
-    }
+}
 
 ?>
 <?php output($entries);?>
@@ -98,15 +103,14 @@ for($col = 0; $col < 3; $col++){
     ['0', '0', '0'],
     ['0', '0', '0']
 ];
-for($col = 0; $col < 3; $col++){
-    for($row = 0; $row < 3; $row++){
-        if($row===1 && $col>=0){
-        continue;
+for ($col = 0; $col < 3; $col++) {
+    for ($row = 0; $row < 3; $row++) {
+        if ($row === 1 && $col >= 0) {
+            continue;
         }
-        $entries[$row][$col] ='x';}
-
-        
-    }
+        $entries[$row][$col] ='x';
+    }       
+}
 
 ?>
 <?php output($entries);?>
@@ -122,15 +126,14 @@ for($col = 0; $col < 3; $col++){
     ['0', '0', '0'],
     ['0', '0', '0']
 ];
-for($col = 0; $col < 3; $col++){
-    for($row = 0; $row < 3; $row++){
-        if($row===2 && $col>=1){
-        continue;
+for ($col = 0; $col < 3; $col++) {
+    for ($row = 0; $row < 3; $row++) {
+        if ($row === 2 && $col >= 1) {
+            continue;
         }
-        $entries[$row][$col] ='x';}
-
-        
-    }
+        $entries [$row][$col] ='x';
+    } 
+}
 
 ?>
 <?php output($entries);?>
@@ -144,15 +147,16 @@ for($col = 0; $col < 3; $col++){
     ['0', '0', '0'],
     ['0', '0', '0']
 ];
-for($col = 0; $col < 3; $col++){
-    for($row = 0; $row < 3; $row++){
-        if($row>=1 && $col>=1){
-        continue;
+for ($col = 0; $col < 3; $col++) {
+    for ($row = 0; $row < 3; $row++) {
+        if ($row>=1 && $col>=1) {
+            continue;
+        }
+        $entries[$row][$col] ='x';
     }
-        $entries[$row][$col] ='x';}
 
         
-    }
+}
 
 ?>
 <?php output($entries);?>
@@ -166,17 +170,17 @@ for($col = 0; $col < 3; $col++){
     ['0', '0', '0'],
     ['0', '0', '0']
 ];
-for($col = 0; $col < 3; $col++){
-    for($row = 0; $row < 3; $row++){
-        if($row===0 && $col===1 ||$row===2 && $col===1 ||$row===1)
-{
-        continue;
+for ($col = 0; $col < 3; $col++){
+    for ($row = 0; $row < 3; $row++){
+        if ($row === 0 && $col === 1 ||$row === 2 && $col === 1 ||$row === 1) {
+            continue;
         }
     
-        $entries[$row][$col] ='x';}
+        $entries[$row][$col] ='x';
+    }
 
         
-    }
+}
 
 ?>
 <?php output($entries);?>
@@ -191,21 +195,17 @@ for($col = 0; $col < 3; $col++){
     ['0', '0', '0']
 ];
 
-for($col = 0; $col < 3; $col++){
-    for($row = 0; $row < 3; $row++){
-  
-        if($row===0 && $col>0 || $row===1||$row===2 && $col>0 )
-           
-        {
-           
-        continue;
+for ($col = 0; $col < 3; $col++) {
+    for ($row = 0; $row < 3; $row++) {  
+        if ($row === 1 && $col >= 0) {
+            $entries[$row][$col] ='m';   
+        }
+        if ($row === 0 && $col > 0 || $row === 1 || $row === 2 && $col > 0 ) {
+           continue;  
+        }
         
-         }
-         $entries[1][0]=$entries[1][1]=$entries[1][2]='m';
         $entries[$row][$col] ='x';
-    }
-
-        
+    }        
 }
 
 ?>
@@ -224,22 +224,66 @@ for($col = 0; $col < 3; $col++){
     ['0', '0', '0']
 ];
 
-for($col = 0; $col < 3; $col++){
-    for($row = 0; $row < 3; $row++){
-  
-        if($row===0 && $col>0 || $row===1||$row===2 && $col>0 )
-           
-        {
-           
-        continue;
-        
-         }
-         $entries[1][0]=$entries[1][1]=$entries[1][2]='m';
-        $entries[$row][$col] ='x';
-    }
 
-        
-}
+
+$counter_1=6;
+$counter_2=5;
+
+for ($col = 0; $col < 3; $col++) {
+    for ($row = 0; $row < 3; $row++){
+        if ($col === 1){
+            continue;
+        }
+        if ($col === 0 && $row >=0 && $counter_1 >= 2){
+            $entries[$row][$col] =$counter_1; 
+            $counter_1= $counter_1 - 2;
+        }
+        if ($col === 2 && $row >=0 && $counter_2 >= 1){
+            $entries[$row][$col] =$counter_2; 
+            $counter_2 = $counter_2 - 2;
+        }            
+    }
+   
+}     
+
+
+?>
+<?php output($entries);?>
+</div>
+
+<br>
+<br>
+<div style="width:100px;display:inline-block;">
+        <h1>10)</h1>
+        <?php
+ $entries= [
+    ['0', '0', '0'],
+    ['0', '0', '0'],
+    ['0', '0', '0']
+];
+
+
+
+$counter_1=8;
+$counter_2=5;
+
+for ($col = 0; $col < 3; $col++) {
+    for($row = 0; $row < 3; $row++){
+        if($col === 1){
+            continue;
+        }
+        if($col === 0 && $row >=0 && $counter_1 >= 2){
+            $entries[$row][$col] =$counter_1; 
+            $counter_1= $counter_1 - 2;
+        }
+        if($col === 2 && $row >=0 && $counter_2 >= 1){
+            $entries[$row][$col] =$counter_2; 
+            $counter_2 = $counter_2 - 2;
+        }            
+    }
+   
+}     
+
 
 ?>
 <?php output($entries);?>
